8e46c4c36c7f660dbfc3d93c4f6f606c4f853c5c
manav9953420551 <46513245+manav9953420551@users.noreply.github.com>
2020-05-07 01:09:49 +0530

    Update librarianController.js

--- controllers/librarianController.js
+++ controllers/librarianController.js
@@ -30,7 +30,7 @@ var mailOptions = {
   to: 'dtaneja089@gmail.com',
   subject: 'Verification mail',
  // text: 'http://localhost:3000/show'
-  html:"<h1>Hello Guys</h1><br><p>verification from .. system</p><br><a href='http://52.14.235.218:3000/show'>click me</a>"
+  html:"<h1>Hello Guys</h1><br><p>verification from .. system</p><br><a href='http://http://18.221.127.157:3000//show'>click me</a>"
 
 };
 
